e482daa23854bee0728d87c5c673014db05f8d38
tram_zulassung <1042749444@qq.com>
2023-03-29 20:09:03 +0800

    openWeather API添加

--- 实时天气api/assets/js/api.js
+++ 实时天气api/assets/js/api.js
@@ -4,3 +4,42 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+"use strict";
+
+const api_key = "c76a591351beda27a20396fd4780ac8c";
+
+/**
+ * 后端数据接口
+ * @param {string} URL  API url
+ * @param {Function} callback callback 
+ */
+
+export const fetchData =  function(URL,callback) {
+    fetchData(`${URL}&appid=${api_key}`)
+    .then(res=>res.json())
+    .then(data=> callback(data))
+}
+
+export const url = {
+    currentWeather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+    },
+    forecast(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+    },
+    airPollution(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+    },
+    resverGeo(lat,lon){
+        return `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${api_key}`
+    },
+    /**
+     * 
+     * @param {string} query  search query exp:"Lodon","New York" 
+     * @returns 
+     */
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${api_key}`
+    }
+}
