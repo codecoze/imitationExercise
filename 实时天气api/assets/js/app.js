e3689c1f28c9c3d06a12df849d00b659e6a01270
tram_zulassung <1042749444@qq.com>
2023-03-29 21:05:58 +0800

    搜索框展开效果添加

--- 实时天气api/assets/js/app.js
+++ 实时天气api/assets/js/app.js
@@ -4,3 +4,27 @@
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
 
+"use strict";
+
+import { fetchData,url } from "./api.js";
+import * as module from "./module.js";
+
+/**
+ * Add event listener on multiple elements
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType event type exp: 'click'
+ * @param {function} callback  callback function
+ */
+const addEventOnElements = function (elements,eventType, callback) {
+    for(const element of elements)element.addEventListener(eventType, callback);
+}
+
+/**
+ * Toggle search in mobile devices
+ */
+
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers,"click",toggleSearch);
