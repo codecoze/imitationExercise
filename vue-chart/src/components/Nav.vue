41de1aa24bae9a3bfc23ae7a6e63d89e7c471a27
tram_zulassung <1042749444@qq.com>
2023-03-31 19:09:06 +0800

    模拟聊天记录页面

---
+++ vue-chart/src/components/Nav.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="nav">
+        <div class="nav-menu-wrapper">
+            <ul class="memu-list">
+
+            </ul>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import { ref,createRouter } from "vue";
+
+const menuList = ref([]);
+const current = ref(0);
+
+const imgUrl = ref(null);
+
+imgUrl.value = require("@/assets/img/head_portrait.jpg")
+
+menuList.value = [
+        "icon-xinxi",
+        "icon-shipin",
+        "icon-shu",
+        "icon-shandian",
+        "icon-shezhi",];
+
+
+function changeMenu(index){
+    switch (index) {
+        case 0:
+          this.$router.push({
+            name: "ChatHome",
+          }, () => {});
+          break;
+        case 1:
+          this.$message("该功能还没有开发哦，敬请期待一下吧~🥳");
+          break;
+        case 2:
+          this.$message("该功能还没有开发哦，敬请期待一下吧~🥳");
+          break;
+        case 3:
+          this.$message("该功能还没有开发哦，敬请期待一下吧~🥳");
+          break;
+        case 4:
+          this.$message("该功能还没有开发哦，敬请期待一下吧~🥳");
+          break;
+        default:
+          this.$router.push({
+            name: "ChatHome",
+          });
+    }
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
