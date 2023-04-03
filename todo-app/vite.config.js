244db3760e778cc5d0e26a903c3137ea1161615c
tram_zulassung <1042749444@qq.com>
2023-04-03 15:22:29 +0800

    vite 配置别名

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import { createHtmlPlugin } from "vite-plugin-html"
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import path from "path";
 // 为了在html中使用env变量
  // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
@@ -24,7 +25,7 @@ export default defineConfig( ({ command, mode}) => {
     ],
     resolve: {
       alias: {
-        
+        "@": path.resolve(__dirname,"./src"),
       }
     }
   
