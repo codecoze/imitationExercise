0c3389af8b61dcd2f96da51e75a37020d6c1f5a9
tram_zulassung <1042749444@qq.com>
2023-04-03 13:09:09 +0800

    配置系统标题

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from "vite-plugin-html"
 import vue from '@vitejs/plugin-vue'
+// 为了在html中使用env变量
+ // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+const getViteEnv = (mode,target) =>{
+  return loadEnv(mode,process.cwd())[target];
+}
+//const env = loadEnv(mode, process.cwd(), '')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig( ({ command, mode}) => {
+  return  {
+    plugins: [
+      vue(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: getViteEnv(mode,"VITE_APP_TITLE"), //将环境变量 VITE_APP_TITLE 赋值给 title 方便 html页面使用 title 获取系统标题
+          }
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        
+      }
+    }
+  
+  }  
 })
